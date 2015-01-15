e7b7b9f46ef4b70e7163c4c2346044ce81df5253
jakob_f <jakob.frohnwieser@gmx.at>
2015-01-15 19:44:25 +0100

    updates/ontology

--- Master/webapp-api/src/main/java/at/frohnwieser/mahut/webappapi/ontology/OntologyManager.java
+++ Master/webapp-api/src/main/java/at/frohnwieser/mahut/webappapi/ontology/OntologyManager.java
@@ -3,16 +3,16 @@ package at.frohnwieser.mahut.webappapi.ontology;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
@@ -34,8 +34,6 @@ public class OntologyManager {
 	    final OWLOntologyManager aManager = OWLManager.createOWLOntologyManager();
 	    m_aFactory = aManager.getOWLDataFactory();
 	    m_aOntology = aManager.loadOntologyFromOntologyDocument(new File("src/test/resources/animals.owl"));
-	    // TODO
-	    final IRI documentIRI = aManager.getOntologyDocumentIRI(m_aOntology);
 	    m_aPM = new DefaultPrefixManager("http://nlp.shef.ac.uk/abraxas/ontologies/animals.owl");
 	} catch (final OWLOntologyCreationException e) {
 	    e.printStackTrace();
@@ -48,29 +46,24 @@ public class OntologyManager {
 
     @Nonnull
     public Collection<String> getEqualClasses(@Nullable final String sClass) {
-	final Collection<String> aRet = new ArrayList<String>();
-
-	// TODO make case insensitive
 	if (StringUtils.isNotEmpty(sClass)) {
-	    final OWLClass aFound = m_aFactory.getOWLClass(CLASS_PREFIX + sClass, m_aPM);
-	    final Collection<OWLClassExpression> aSuperClasses = EntitySearcher.getSuperClasses(aFound, m_aOntology);
-	    if (CollectionUtils.isNotEmpty(aSuperClasses))
-		for (final OWLClassExpression aSuperClass : aSuperClasses) {
-		    final Collection<OWLClassExpression> aSubClasses = EntitySearcher.getSubClasses(aSuperClass.asOWLClass(), m_aOntology);
-		    aSubClasses.stream().forEach(aSubClass -> aRet.add(aSubClass.asOWLClass().getIRI().getShortForm()));
-		}
+	    // TODO make case insensitive
+	    final String sClassCasesenstive = WordUtils.capitalizeFully(sClass);
+	    final OWLClass aFound = m_aFactory.getOWLClass(CLASS_PREFIX + sClassCasesenstive, m_aPM);
+	    // TODO check if class exists
+	    return EntitySearcher.getSuperClasses(aFound, m_aOntology).stream()
+		    .flatMap(aSuperClass -> EntitySearcher.getSubClasses(aSuperClass.asOWLClass(), m_aOntology).stream())
+		    .map(aSubClass -> aSubClass.asOWLClass().getIRI().getShortForm()).collect(Collectors.toCollection(ArrayList::new));
 	}
 
-	return aRet;
+	return new ArrayList<String>();
     }
 
     @Nonnull
     public Collection<String> getEqualClasses(@Nullable final Collection<String> aClasses) {
-	final Collection<String> aRet = new ArrayList<String>();
-
 	if (CollectionUtils.isNotEmpty(aClasses))
-	    aClasses.stream().forEach(sClass -> aRet.addAll(getEqualClasses(sClass)));
+	    return aClasses.stream().flatMap(sClass -> getEqualClasses(sClass).stream()).collect(Collectors.toCollection(ArrayList::new));
 
-	return aRet;
+	return new ArrayList<String>();
     }
 }
--- Master/webapp-api/src/test/java/at/frohnwieser/mahut/webappapi/ontology/OntologyManagerTest.java
+++ Master/webapp-api/src/test/java/at/frohnwieser/mahut/webappapi/ontology/OntologyManagerTest.java
@@ -10,7 +10,7 @@ public class OntologyManagerTest {
     @Test
     public void testGetSubclasses() {
 	final Collection<String> aList = new ArrayList<String>();
-	aList.add("Spider");
+	aList.add("lion");
 
 	OntologyManager.getInstance().getEqualClasses(aList).forEach(System.out::println);
     }
