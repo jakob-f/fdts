68769fc1fe66f1ab60f0e429fbfbf9fa1f80ce4a
jakob_f <jakob.frohnwieser@gmx.at>
2015-09-12 15:34:03 +0200

    fixes

--- Master/ffmpeg-wrapper/src/main/java/at/frohnwieser/mahut/ffmpegwrapper/util/EFormat.java
+++ Master/ffmpeg-wrapper/src/main/java/at/frohnwieser/mahut/ffmpegwrapper/util/EFormat.java
@@ -4,34 +4,37 @@ import javax.annotation.Nonnull;
 
 public enum EFormat {
     OGG("ogg",
-            "libvorbis",
-            "libtheora"),
+	    "libvorbis",
+	    "libtheora"),
     MP4("mp4",
-            "libfaac",
-            "libx264"),
+	    "libfaac",
+	    "libx264"),
     WEBM("webm",
-            "libvorbis",
-            "libvpx");
+	    "libvorbis",
+	    "libvpx"),
+    MAC_MP4("mp4",
+	    "libfdk_aac",
+	    "libx264");
 
     private final String f_sName;
     private final String f_sACodec;
     private final String f_sVCodec;
 
     private EFormat(@Nonnull final String sName, @Nonnull final String sACodec, @Nonnull final String sVCodec) {
-        f_sName = sName;
-        f_sACodec = sACodec;
-        f_sVCodec = sVCodec;
+	f_sName = sName;
+	f_sACodec = sACodec;
+	f_sVCodec = sVCodec;
     }
 
     public String getName() {
-        return f_sName;
+	return f_sName;
     }
 
     public String getACodec() {
-        return f_sACodec;
+	return f_sACodec;
     }
 
     public String getVCodec() {
-        return f_sVCodec;
+	return f_sVCodec;
     }
 }
